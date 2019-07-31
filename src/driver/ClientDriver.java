73f239e5f072a50f4760a6f880cbda94ebb9b8d0
Drabblesaur <johnnyto4285@gmail.com>
2019-07-31 16:15:37 -0700

    Update ClientDriver.java

--- src/driver/ClientDriver.java
+++ src/driver/ClientDriver.java
@@ -4,7 +4,7 @@ import client.DefaultSocketClient;
 
 public class ClientDriver {
     public static void main(String [] args){
-        DefaultSocketClient client = new DefaultSocketClient("10.41.239.77",3220);
+        DefaultSocketClient client = new DefaultSocketClient("IP ADDRESS","PORT");
         client.run();
     }
 }
